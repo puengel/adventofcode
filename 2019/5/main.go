fb638af5072dbe29de23183e089758ec90d8e4d8
puengel <daniel.puengel@gmail.com>
2020-06-22 22:10:04 +0200

    2019 day 5 done

--- 2019/5/main.go
+++ 2019/5/main.go
@@ -16,7 +16,8 @@ func main() {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("in: %+v\n", in)
+	// fmt.Printf("in: %+v\n", in)
+	operate(in)
 
 }
 
@@ -41,28 +42,157 @@ func readInput() ([]int, error) {
 	return iVars, err
 }
 
-
-
 func operate(input []int) (output []int, err error) {
 	output = make([]int, len(input))
 	copy(output, input)
 
-	fmt.Print(output)
+	// fmt.Print(output)
 	finished := false
-	for i := 0; !finished {
+	for i := 0; !finished; {
 
-		switch output[i] {
+		opcode := output[i]
+		DE := opcode % 100
+		C := (opcode / 100) % 10
+		B := (opcode / 1000) % 10
+		A := (opcode / 10000) % 10
+		// fmt.Println(opcode)
+
+		switch DE {
 		case 1:
 			// add
-			output[output[i+3]] = output[output[i+1]] + output[output[i+2]]
+			p1 := output[i+1]
+			if C == 0 {
+				p1 = output[output[i+1]]
+			}
+			p2 := output[i+2]
+			if B == 0 {
+				p2 = output[output[i+2]]
+			}
+			if A == 0 {
+				output[output[i+3]] = p1 + p2
+			} else {
+				output[i+3] = p1 + p2
+			}
 			i += 4
 		case 2:
 			// multiply
-			output[output[i+3]] = output[output[i+1]] * output[output[i+2]]
+			p1 := output[i+1]
+			if C == 0 {
+				p1 = output[output[i+1]]
+			}
+			p2 := output[i+2]
+			if B == 0 {
+				p2 = output[output[i+2]]
+			}
+			if A == 0 {
+				output[output[i+3]] = p1 * p2
+			} else {
+				output[i+3] = p1 * p2
+			}
 			i += 4
 		case 3:
+			// Store input
 
+			var input int
+			fmt.Print("Input: ")
+			_, err := fmt.Scanf("%d", &input)
+			if err != nil {
+				return nil, err
+			}
+
+			if C == 0 {
+				output[output[i+1]] = input
+			} else {
+				output[i+1] = input
+			}
+			i += 2
 		case 4:
+			// Write output
+
+			p1 := output[i+1]
+			if C == 0 {
+				p1 = output[output[i+1]]
+			}
+			fmt.Printf("%d\n", p1)
+			i += 2
+
+		case 5:
+			// Jump if true
+			p1 := output[i+1]
+			if C == 0 {
+				p1 = output[output[i+1]]
+			}
+
+			if p1 != 0 {
+				p2 := output[i+2]
+				if B == 0 {
+					p2 = output[output[i+2]]
+				}
+				i = p2
+			} else {
+				i += 3
+			}
+		case 6:
+			// Jump if false
+			p1 := output[i+1]
+			if C == 0 {
+				p1 = output[output[i+1]]
+			}
+
+			if p1 == 0 {
+				p2 := output[i+2]
+				if B == 0 {
+					p2 = output[output[i+2]]
+				}
+				i = p2
+			} else {
+				i += 3
+			}
+
+		case 7:
+			// less than
+			p1 := output[i+1]
+			if C == 0 {
+				p1 = output[output[i+1]]
+			}
+			p2 := output[i+2]
+			if B == 0 {
+				p2 = output[output[i+2]]
+			}
+			p3 := output[i+3]
+			// if A == 0 {
+			// 	p3 = output[output[i+3]]
+			// }
+			if p1 < p2 {
+				output[p3] = 1
+			} else {
+				output[p3] = 0
+			}
+			i += 4
+
+		case 8:
+			// equals
+			p1 := output[i+1]
+			if C == 0 {
+				p1 = output[output[i+1]]
+			}
+			p2 := output[i+2]
+			if B == 0 {
+				p2 = output[output[i+2]]
+			}
+			p3 := output[i+3]
+			// if A == 0 {
+			// 	p3 = output[output[i+3]]
+			// }
+			if p1 == p2 {
+				output[p3] = 1
+				// fmt.Println("blubb")
+			} else {
+				output[p3] = 0
+				// fmt.Println("oj")
+			}
+			// fmt.Println(output)
+			i += 4
 
 		case 99:
 			finished = true
