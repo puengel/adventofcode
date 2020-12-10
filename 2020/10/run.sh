5f3dba7894e19a0a58270fbc20355711310e159e
puengel <daniel.puengel@gmail.com>
2020-12-10 17:36:49 +0100

    2020-10 bash

--- 2020/10/run.sh
+++ 2020/10/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-adapters=()
+adapters=(0)
 
-while read line; do adapters+=($line); done < input.txt
+for line in  $(<input.txt); do
+    adapters+=($line)
+done
+
 
 # for a in ${adapters[@]}; do
     # echo $a
@@ -12,4 +15,34 @@ while read line; do adapters+=($line); done < input.txt
 
 IFS=$'\n' sorted=($(sort -n <<<"${adapters[*]}")); unset IFS
 
-echo ${sorted[@]}
+sorted+=($((sorted[-1]+3)))
+
+let before=0 diff1=0 diff3=0
+for n in ${sorted[@]}; do
+    if [[ $((n-before)) -eq 1 ]]
+    then
+        let diff1+=1
+    elif [[ $((n-before)) -eq 3 ]]
+    then
+        let diff3+=1
+    fi
+    before=$n
+done
+
+echo "Part 1: $(($diff1*$diff3))"
+
+# https://www.geeksforgeeks.org/count-ways-reach-nth-stair/
+
+res=(1)
+for i in $(seq 1 $((${#sorted[@]}-1))); do
+    ans=0
+    for j in $(seq 0 $i); do
+        if [[ $((sorted[j]+3)) -ge ${sorted[$i]} ]]
+        then
+            let ans+=res[j]
+        fi
+    done
+    res+=($ans)
+done
+
+echo "Part 2: ${res[-1]}"
